1380c8247457d957ff16116661364c48a8c9dabd
Thomas Kienlen <thomas.kienlen@lafourmi-immo.com>
2013-05-13 14:26:57 +0200

    rbenv + chruby

--- lib/bin/init-ruby.sh
+++ lib/bin/init-ruby.sh
@@ -9,13 +9,35 @@ if ! check_command rbenv; then
   if [[ ! $RBENV_PATH ]]; then
     RBENV_PATH="$HOME/.rbenv"
   fi
-  log "RBENV_PATH : $RBENV_PATH"
+  if [ -d $RBENV_PATH ]; then
+    log "RBENV_PATH : $RBENV_PATH"
+    run_quietly "[ -d $RBENV_PATH ] && export PATH=\"$RBENV_PATH/bin:$PATH\" && eval \"\$(rbenv init - sh)\" && rbenv rehash"
 
-  run_quietly "[ -d $RBENV_PATH ] && export PATH=\"$RBENV_PATH/bin:$PATH\" && eval \"\$(rbenv init - sh)\" && rbenv rehash"
+    # re-check
+    if ! check_command rbenv; then
+      exit_with_error "rbenv"
+    fi
+  fi
+fi
 
-  # re-check
-  if ! check_command rbenv; then
-    exit_with_error "rbenv"
+# check for chruby
+if ! check_command chruby; then
+  CHRUBY_LOADER=/usr/local/share/chruby/chruby.sh
+  CHRUBY_AUTO=/usr/local/share/chruby/auto.sh
+
+  if [ -f $CHRUBY_LOADER ]; then
+    log "LOADING CHRUBY"
+    run_quietly "source $CHRUBY_LOADER"
+    if [ -f $CHRUBY_AUTO ]; then
+      log "LOADING CHRUBY AUTO"
+      run_quietly "source $CHRUBY_AUTO"
+      run_quietly "chruby `cat .ruby-version`"
+    fi
+
+    # re-check
+    if ! check_command chruby; then
+      exit_with_error "chruby"
+    fi
   fi
 fi
 
